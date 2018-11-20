8ba7721c794fc7c44c2f026183a14902c0ce3d3c
Arif Hossain <arifthecoder@gmail.com>
2018-11-20 22:15:34 +0200

    updated

--- Fullstack application with react and django/backend/djreact/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!F:\projects\pythonProject\My-Python-Practice-Files\Fullstack application with react and django\backend\djreact\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
