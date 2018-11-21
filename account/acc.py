f9b0ee2021a2bf504e8c710a2082dbccdd0068b3
Arif Hossain <arifthecoder@gmail.com>
2018-11-21 15:26:18 +0200

    account file was updated

--- account/acc.py
+++ account/acc.py
@@ -18,8 +18,25 @@ class Account:
             file.write(str(self.balance))
 
 
-account = Account("balance.txt")
-print(account.balance)
-account.withdraw(270)
-print(account.balance)
-account.commit()
+class Checking(Account):
+    type = "Checking"
+
+    def __init__(self, filePath, fee):
+        Account.__init__(self, filePath)
+        self.fee = fee
+
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+
+
+tom_checking = Checking("tom.txt", 2)
+tom_checking.transfer(320)
+tom_checking.commit()
+print(tom_checking.balance)
+print(tom_checking.type)
+
+jerry_checking = Checking("jerry.txt", 2)
+jerry_checking.transfer(120)
+jerry_checking.commit()
+print(jerry_checking.balance)
+print(jerry_checking.type)
