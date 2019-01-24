8df899ab687766d6a095c3c3a3bc4e6394fc4aa5
Arif Hossain <arifthecoder@gmail.com>
2019-01-24 21:15:24 +0200

    video capturing with openCV

--- Image and openCV/script1.py
+++ Image and openCV/script1.py
@@ -8,6 +8,6 @@ for image in images:
     img = cv2.imread(image, 0)
     re = cv2.resize(img, (100, 100))
     cv2.imshow('hello', re)
-    cv2.waitKey(4000)
+    cv2.waitKey(1000)
     cv2.destroyAllWindows()
     cv2.imwrite('images//resized_' + image, re)
